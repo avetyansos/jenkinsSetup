cd1e6642a38395fc1ef6c056d712d9ab424d7894
Sos Avetyan <avetyansos@gmail.com>
2018-05-15 16:14:54 +0400

    Initial Commit

---
+++ JenkinsSetup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JenkinsSetup
+//
+//  Created by Sos Avetyan on 5/15/18.
+//  Copyright © 2018 Sos Avetyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
